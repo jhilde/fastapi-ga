5034ca46090896c245424d6cb8929db8d2b26b56
Justin Hildebrandt <justin.hildebrandt@ellevationeducation.com>
2023-12-30 23:18:38 +0000

    okay

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,4 @@
 def test_main(test_client):
     response = test_client.get("/")
     assert response.status_code == 200
-    assert response.json() == {
-        "message": "Hello Bigger Application!"
-    }
+    assert response.json() == {"message": "Hello Bigger Application!"}
